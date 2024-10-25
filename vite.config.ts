9e3850f19670e5b585e87b77fc310bdcea3663c9
hashir <hashirmuzaffar23@gmail.com>
2024-10-26 00:24:46 +0500

    Second

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://github.com/hashirmuzaffar/my-react-app/', // Replace with your GitHub repository name
 })
